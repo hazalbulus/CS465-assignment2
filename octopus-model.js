1de5a0f394396ad988aae9ad0a517c0e0b67be77
Hazal Buluş <hazal.bulus@ug.bilkent.edu.tr>
2023-11-14 20:21:15 +0300

    uodate

--- octopus-model.js
+++
@@ -1,9 +0,0 @@
-function createOctopusModel(gl) {
-    // Define vertices, colors, and indices for the octopus head and tentacles
-    var vertices = [/* ... */];
-    var colors = [/* ... */];
-    var indices = [/* ... */];
-
-    // Code to create and bind VBOs and IBOs
-    // Code to assign data to attributes and uniforms
-}
